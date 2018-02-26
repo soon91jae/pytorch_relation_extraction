44dc1a0cba05a14f8b9e71d729b39d7721ef9781
mozi <soon91jae@unist.ac.kr>
2018-02-26 15:43:57 +0900

    02.26

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[64]:
 
 
 #! /usr/bin/env python
@@ -18,7 +18,7 @@ import random
 from nltk.tokenize import word_tokenize
 
 
-# In[77]:
+# In[65]:
 
 
 def create_one_batch(ids, x, y):
@@ -33,7 +33,7 @@ def create_batches(x, y, batch_size, shuffle = True):
     perm = list(range(len(y)))
     if shuffle:
         random.shuffle(perm)
-    
+
     # sort sequences based on their length
     # permutation is necessary if we want different batches every epoch
     lst = sorted(perm, key=lambda i: len(x[i][0]))
@@ -62,7 +62,7 @@ def create_batches(x, y, batch_size, shuffle = True):
     return batches_x, batches_y
 
 
-# In[78]:
+# In[66]:
 
 
 def tokenizer(text):
@@ -72,7 +72,7 @@ def tokenizer(text):
     return " ".join(word_tokenize(text))
 
 
-# In[79]:
+# In[67]:
 
 
 def data_split(datas, test_rate = 0.2):
@@ -81,7 +81,7 @@ def data_split(datas, test_rate = 0.2):
     return datas[:pivot_index], datas[pivot_index:]
 
 
-# In[80]:
+# In[68]:
 
 
 
@@ -138,10 +138,11 @@ def SemEval10_task8(sub_path, path = './dataset/SemEval2010_task8_all_data/'):
     
 
 
-# In[90]:
+# In[69]:
 
 
-def preprocess_dataset(datas, MAX_POS = 15, label_factorize = False):
+"""
+def preprocess_dataset(datas, MAX_POS = 15, label_factorize = False, entity_normalize = False):
     sents = []
     position_indices_1= []
     position_indices_2= []
@@ -155,10 +156,12 @@ def preprocess_dataset(datas, MAX_POS = 15, label_factorize = False):
         e1_index = e2_index = None
         for i, word in enumerate(words):
             if word == 'E1':
-                words[i] = e1
+                if not entity_normalize:
+                    words[i] = e1
                 e1_index = i
             elif word == 'E2':
-                words[i] = e2
+                if not entity_normalize:
+                    words[i] = e2
                 e2_index = i
         sents.append(words)
         #print(words)
@@ -188,35 +191,145 @@ def preprocess_dataset(datas, MAX_POS = 15, label_factorize = False):
     
     return list(zip(sents, position_indices_1, position_indices_2)), labels
     #return {'sents': sents, 'position_indices_1': position_indices_1, 'position_indices_2': position_indices_2, 'labels': labels}
+"""
 
 
-# In[91]:
+# In[70]:
+
+
+def preprocess_dataset(datas, MAX_POS = 15, label_factorize = False, entity_normalize = False, directional_consideration = True):
+    sents = []
+    position_indices_1= []
+    position_indices_2= []
+    labels = []
+    for data in datas:
+        # Get content data from input data
+        sentence, e1, e2 = data['content']
+        
+        # Change entities as a original word
+        words = sentence.split()
+        e1_index = e2_index = None
+        e1_len = e2_len = None
+        input_words = []
+        
+        index = 0
+        for word in words:
+            #print(input_words)
+            if word == 'E1':
+                if entity_normalize:
+                    input_words.append('e1')
+                    index += 1
+                else:
+                    
+                    tokens = e1.strip().split(' ')
+                    
+                    input_words += ['<e1>']
+                    for token in tokens:
+                        
+                        input_words += [token]
+                    input_words += ['</e1>']
+                    e1_index = index
+                    e1_len = len(tokens)+2
+                    index += e1_len
+            elif word == 'E2':
+                if entity_normalize:
+                    input_words.append('e2')
+                    index += 1
+                else:
+                    
+                    tokens = e2.strip().split(' ')
+                    
+                    input_words += ['<e2>']
+                    for token in tokens:
+                        input_words += [token]
+                    input_words += ['</e2>']
+                    e2_index = index
+                    e2_len = len(tokens)+2
+                    index += e2_len
+            else:
+                input_words += [word]
+                index += 1
+        sents.append(input_words)
+        #print(input_words)
+        #print(words)
+        
+        # Get position embedding index
+        position_index_1 = []
+        position_index_2 = []
+        for i in range(len(input_words)):
+            
+            if i < e1_index:
+                pos1 =   i - e1_index
+            elif i >= e1_index + e1_len:
+                pos1 = i - (e1_index + e1_len - 1)
+            else:
+                pos1 = 0
+            
+            
+            if i < e2_index:
+                pos2 =   i - e2_index
+            elif i >= e2_index + e2_len:
+                pos2 = i - (e2_index + e2_len - 1)
+            else:
+                pos2 = 0
+            #print(pos2)
+            position_index_1.append(int(MAX_POS * abs(pos1) / (pos1)) if abs(pos1) > MAX_POS else pos1)
+            position_index_2.append(int(MAX_POS * abs(pos2) / (pos2)) if abs(pos2) > MAX_POS else pos2)
+            #position_index_2.append(pos2 + MAX_POS)
+            #position_index_1.append(pos1 + MAX_POS)
+            #print(input_words[e1_index:e1_index+e1_len])
+            #print(input_words[e2_index:e2_index+e2_len])
+            
+            
+            position_index_1[i] += MAX_POS
+            position_index_2[i] += MAX_POS
+        position_indices_1.append(position_index_1)
+        position_indices_2.append(position_index_2)
+        #print(position_index_1)
+        #print(position_index_2)
+        
+        # Make label
+        if not label_factorize:
+            #print(data['category'])
+            label, t1, t2 = data['category']
+            if label != 'Other':
+                if directional_consideration:
+                    labels.append(label + '(e' + t1+',e'+ t2+')')
+                else:
+                    labels.append(label)
+            else:
+                labels.append(label)
+    
+    return list(zip(sents, position_indices_1, position_indices_2)), labels
+
+
+# In[71]:
 
 
 #train = SemEval10_task8(sub_path='SemEval2010_task8_training/TRAIN_FILE.TXT')
 #train_data = preprocess_dataset(train)
 
 
-# In[92]:
+# In[72]:
 
 
 #train_input = train_data[0]
 #train_output = train_data[1]
 
 
-# In[93]:
+# In[73]:
 
 
 #train_input[0]
 
 
-# In[94]:
+# In[74]:
 
 
 #batch_x, batch_y = create_batches(train_input, train_output, 16)
 
 
-# In[95]:
+# In[75]:
 
 
 #print(len(batch_y[0]))
@@ -225,4 +338,10 @@ def preprocess_dataset(datas, MAX_POS = 15, label_factorize = False):
 #print(len(batch_x))
 #print(batch_x[0])
 #print(batch_y[0])
+
+
+# In[76]:
+
+
+#torch.Longtensor([1,2,3,4,5])
 
